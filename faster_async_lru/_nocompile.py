e7a83a5ff59d7722cc393e0983e37bd83f657be6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-12-10 06:39:07 +0000

    fix: generic

--- faster_async_lru/_nocompile.py
+++ faster_async_lru/_nocompile.py
@@ -1,12 +1,14 @@
-from typing import Generic, TypeVar
+from typing import Generic
+
+from typing_extensions import TypeVarTuple, Unpack
 
 
 __all__ = ("_NonNativeClass",)
 
 
-_R = TypeVar("_R")
+_T = TypeVarTuple("_")
 
-class _NonNativeClass(Generic[_R]):
+class _NonNativeClass(Generic[Unpack[_T]]):
     """This class only exists as a workaround to the fact
     that native classes have no __dict__, but we need our
     wrappers to have it.
