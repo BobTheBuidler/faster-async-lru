107e65fc23a811f0d0c119138ac0429152311761
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-12-10 06:41:35 +0000

    fix: type var name

--- faster_async_lru/_nocompile.py
+++ faster_async_lru/_nocompile.py
@@ -6,10 +6,19 @@ from typing_extensions import TypeVarTuple, Unpack
 __all__ = ("_NonNativeClass",)
 
 
-_T = TypeVarTuple("_")
+_T = TypeVarTuple("_T")
 
 class _NonNativeClass(Generic[Unpack[_T]]):
-    """This class only exists as a workaround to the fact
+    """
+    A generic class that can accept any number and type of type arguments.
+
+    Example usage:
+        _NonNativeClass[int]
+        _NonNativeClass[int, str]
+        _NonNativeClass[int, str, float]
+        # etc.
+
+    This class only exists as a workaround to the fact
     that native classes have no __dict__, but we need our
     wrappers to have it.
     """
