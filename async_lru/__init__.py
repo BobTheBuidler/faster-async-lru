894a693411c69922541293d428f74e3bcf300e05
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-18 09:23:04 -0400

    Restore cached attributes in _task_done_callback

--- async_lru/__init__.py
+++ async_lru/__init__.py
@@ -205,11 +205,13 @@ class _LRUCacheWrapper(Generic[_R]):
             self.__cache.pop(key, None)
             return
 
-        cache_item = self.__cache.get(key)
-        if self.__ttl is not None and cache_item is not None:
+        cache = self.__cache
+        cache_item = cache.get(key)
+        ttl = self.__ttl
+        if ttl is not None and cache_item is not None:
             loop = asyncio.get_running_loop()
             cache_item.later_call = loop.call_later(
-                self.__ttl, self.__cache.pop, key, None
+                ttl, cache.pop, key, None
             )
 
     async def _shield_and_handle_cancelled_error(
